f6e424170398024be7695c57bcbc6dc7696d3d30
stevecho0101 <smcho@usc.edu>
2026-02-15 22:00:12 -0800

    Add files via upload

---
+++ Lab4/vm_pub.py
@@ -0,0 +1,59 @@
+"""EE 250L Lab 04 Starter Code
+Run vm_sub.py in a separate terminal on your VM."""
+
+# Steve Cho (USC ID: 4314516349)
+# Sivan Nir (USC ID: 7594069996)
+
+import paho.mqtt.client as mqtt
+import time
+from datetime import datetime
+import socket
+
+"""This function (or "callback") will be executed when this client receives 
+a connection acknowledgement packet response from the server. """
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+if __name__ == '__main__':
+    #get IP address
+    ip_address = "127.0.0.1"
+    #create a client object
+    client = mqtt.Client()
+    
+    #attach the on_connect() callback function defined above to the mqtt client
+    client.on_connect = on_connect
+    """Connect using the following hostname, port, and keepalive interval (in 
+    seconds). We added "host=", "port=", and "keepalive=" for illustrative 
+    purposes. You can omit this in python.
+        
+    The keepalive interval indicates when to send keepalive packets to the 
+    server in the event no messages have been published from or sent to this 
+    client. If the connection request is successful, the callback attached to
+    `client.on_connect` will be called."""
+
+    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+
+    """ask paho-mqtt to spawn a separate thread to handle
+    incoming and outgoing mqtt messages."""
+    client.loop_start()
+    time.sleep(1)
+
+    while True:
+        #replace user with your USC username in all subscriptions
+        client.publish("smcho/ipinfo", f"{ip_address}")
+        print("Publishing ip address")
+        time.sleep(4)
+
+        #get date and time 
+        date_time = datetime.now()
+        t = date_time.time()
+        d = date_time.date()
+
+        #publish date and time in their own topics
+        client.publish("smcho/date", f"Date: {d.month}/{d.day}/{d.year}")
+        print("Publishing Date")
+        time.sleep(4)
+        
+        client.publish("smcho/time", f"Time: {t.hour}:{t.minute}:{t.second}")
+        print("Publishing Time")
+        time.sleep(4)
---
+++ Lab4/vm_start_chain.py
@@ -0,0 +1,48 @@
+"""EE 250L Lab 04 Start Chain"""
+
+# Steve Cho (USC ID: 4314516349)
+# Sivan Nir (USC ID: 7594069996)
+
+import paho.mqtt.client as mqtt
+import time
+
+# Call back function that gets celled when the client and the MQTT broker on RPi is successfully connected
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+    client.subscribe("smcho/pong") #
+    client.message_callback_add("smcho/pong", on_message_from_pong)
+
+
+"""This object (functions are objects!) serves as the default callback for 
+messages received when another node publishes a message this client is 
+subscribed to. By "default,"" we mean that this callback is called if a custom 
+callback has not been registered using paho-mqtt's message_callback_add()."""
+def on_message(client, userdata, msg):
+    print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
+
+# Pong message callback
+def on_message_from_pong(client, userdata, message):
+    num = int(message.payload.decode("utf-8")) # Decode the received message
+    num = num+1 # Increment the number
+    time.sleep(1) # Wait before publishing
+    client.publish("smcho/ping", str(num))# Publish the incremented number to 'smcho/ping' channel
+    print(f"Received Pong {num-1}, Published Ping {num}")
+
+
+if __name__ == '__main__':
+    
+    # Create a client object
+    client = mqtt.Client()
+    # Attach a default callback which we defined above for incoming mqtt messages
+    client.on_message = on_message
+    # Attach the on_connect() callback function defined above to the mqtt client
+    client.on_connect = on_connect
+    # Connect the client to the RPi broker
+    client.connect(host="172.20.10.3", port=1883, keepalive=60)
+    # Initial count value starts from 0 and publishes to the 'smcho/ping' channel to start the communication
+    count = 0
+    time.sleep(1)
+    print(f"Starting Value: {count}")
+    client.publish('smcho/ping', str(count))
+    # Ensures the client loops forever
+    client.loop_forever()
