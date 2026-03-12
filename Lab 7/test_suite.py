1a27a531a1ba9e5364f1c01f12997734649fa306
stevecho0101 <smcho@usc.edu>
2026-03-12 12:08:22 -0700

    Change lux and sound thresholds in test_suite.py
    Updated threshold values for light and sound sensors.

--- Lab 7/test_suite.py
+++ Lab 7/test_suite.py
@@ -17,19 +17,17 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 # appropriate threshold levels and set them 
 # accordingly. Then, use them to determine
 # when it is light or dark, quiet or loud.
-lux_treshold=200  # change this value
-sound_treshold=160 # change this value
+lux_treshold=500  # change this value
+sound_treshold=600 # change this value
 
 
 while True: 
-  # LED blinking 5 times with 500 ms interval between on and off
   for i in range(0, 5):
     time.sleep(0.5)
     GPIO.output(chan_list, GPIO.HIGH)
     time.sleep(0.5) 
     GPIO.output(chan_list, GPIO.LOW)
-
-  # Light sensor reading and printing value every 100 ms for 5 seconds
+  
   for i in range(0, 50):
     val = mcp.read_adc(0)
     print(val)
@@ -38,15 +36,13 @@ while True:
     else:
       print("dark")
     time.sleep(0.1) 
-
-  # LED blinking 4 times with 200 ms interval between on and off
+  
   for i in range(0, 4):
     time.sleep(0.2)
     GPIO.output(chan_list, GPIO.HIGH)
     time.sleep(0.2) 
     GPIO.output(chan_list, GPIO.LOW)
-    
-  # Sound sensor reading every 100 ms for 5 seconds. LED will be turned on for 100 ms everytime the sound magnitude is greater than the threshold. It maintains the total 5 seconds
+  
   for i in range(0, 50):
     val = mcp.read_adc(1)
     print(val)
